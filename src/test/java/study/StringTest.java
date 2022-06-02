84d7e9ebfc77980bdb40f40c069d9af9a14030ed
seominah <seomiinah95@gmail.com>
2022-06-02 22:27:34 +0900

    feat: String charAt test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -16,10 +18,21 @@ public class StringTest {
         String[] result = "1,2".split(",");
         assertThat(result).containsExactly("1", "2");
     }
+    
     @Test
     void substring() {
         String str ="(1,2)";
         String result = str.substring(1, str.length() - 1);
         assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("4번째 위치를 가져오면 StringIndexOutOfBoundsException 에러 발생")
+    void charAt() {
+        assertThatThrownBy(() -> {
+            int result = "abc".charAt(4);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 4");
+
     }
 }
